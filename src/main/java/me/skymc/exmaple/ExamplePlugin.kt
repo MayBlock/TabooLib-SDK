390e3719b3a966bb4e7a5cfca8e25d4f8a4ce21c
sky <sky@asgard.me>
2020-03-08 18:42:30 +0800

    init

---
+++ src/main/java/me/skymc/exmaple/ExamplePlugin.kt
@@ -0,0 +1,26 @@
+package me.skymc.exmaple
+
+import io.izzel.taboolib.loader.Plugin
+import io.izzel.taboolib.loader.PluginRedefine
+import io.izzel.taboolib.module.inject.TInject
+import io.izzel.taboolib.module.locale.logger.TLogger
+
+@Plugin.Version(5.18) // The TabooLib dependency version (required)
+class ExamplePlugin : PluginRedefine() {
+
+    override fun onLoading() {
+        // override onLoad()
+    }
+
+    override fun onStarting() {
+        // override onEnable()
+    }
+
+    override fun onStopping() {
+        // override onDisable()
+    }
+
+    override fun onActivated() {
+        // when the server is fully started
+    }
+}
